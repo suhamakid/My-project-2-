444ea647f8eff96bae040c568adab5b278527979
suhamakid <suhamawake@icloud.com>
2022-02-20 17:42:53 +0900

    ima
    pussyu

--- Assets/URL.cs
+++ Assets/URL.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public class URL : MonoBehaviour
+    {
+        public void onClick()
+        {
+            Application.OpenURL("https://www.instagram.com/whizchillhouse/?hl=ja");
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
